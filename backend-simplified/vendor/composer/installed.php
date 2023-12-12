7b1468fd108cd58c135380d2c03dcadd18bcb118
pan93412 <pan93412@gmail.com>
2023-12-12 15:27:54 +0800

    refactor: Update lockfile

--- backend-simplified/vendor/composer/installed.php
+++ backend-simplified/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'pan93412/std-backend',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'ae49ea6bc08da2785366853f6c132189d61550d4',
+        'reference' => 'd65ddb338e648b0e316eb20766ec6e5b113af518',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'pan93412/std-backend' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'ae49ea6bc08da2785366853f6c132189d61550d4',
+            'reference' => 'd65ddb338e648b0e316eb20766ec6e5b113af518',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
