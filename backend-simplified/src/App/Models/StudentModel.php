bd53d71be1e3e7385da7c8ab9c95b576544455cc
pan93412 <pan93412@gmail.com>
2024-01-17 21:30:58 +0800

    refactor(backend): Mark $id as PrimaryImplicitValue

--- backend-simplified/src/App/Models/StudentModel.php
+++ backend-simplified/src/App/Models/StudentModel.php
@@ -2,15 +2,20 @@
 
 namespace Pan93412\Backend\App\Models;
 
+use Pan93412\Magical\Database\ImplicitValue;
 use Pan93412\Magical\Database\Model;
+use Pan93412\Magical\Database\PrimaryKey;
 
 class StudentModel extends Model
 {
+    #[PrimaryKey]
+    #[ImplicitValue]
+    public int $id;
+
     public string $name;
     public string $email;
     public int $grade;
     public string $birthday;
-    public ?int $id = null;
 
     public static function getTable(): string
     {
